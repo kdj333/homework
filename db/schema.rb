b076b663884549f14c7b6c5275ac29c6acaa3b81
vagrant <vagrant@kellogg-dev-box>
2014-04-20 21:27:42 +0000

    Step 2 Random Dice with Images

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420031601) do
+ActiveRecord::Schema.define(version: 20140420170231) do
 
   create_table "dices", force: true do |t|
     t.integer "number"
+    t.string  "image"
   end
 
 end
