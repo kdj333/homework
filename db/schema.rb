9374bb788071ac86e5812241e1a24d1276b867b9
vagrant <vagrant@kellogg-dev-box>
2014-04-20 22:04:58 +0000

    Step 3 Bookstore Errors Resolved

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420170231) do
+ActiveRecord::Schema.define(version: 20140420220142) do
+
+  create_table "books", force: true do |t|
+    t.string "title"
+    t.string "author"
+  end
 
   create_table "dices", force: true do |t|
     t.integer "number"
